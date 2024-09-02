fe7cea4fefc4df0e808e04b4b5c4434846755294
Imre <rabl.imre95@gmail.com>
2024-09-02 19:47:08 +0200

    added static directory, and fetch data from backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/../frontend/index.html'))
+    res.send("hello world")
 })
+
+app.use('/public', express.static(path.join(__dirname, '/../frontend/public')))
+
+app.get('/data', (req, res) => {
+    res.json({name: "kismacska"})
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ frontend/public/script.js
@@ -0,0 +1,5 @@
+console.log("loaded")
+
+fetch('http://127.0.0.1:3000/data')
+    .then(res => res.json())
+    .then(data => console.log(data))
