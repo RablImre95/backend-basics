a30c5875b9ea4964d8214cd598350f9ca9054c34
Imre <rabl.imre95@gmail.com>
2024-09-04 19:55:18 +0200

    finished data id end point

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,5 +1,13 @@
 console.log("loaded")
 
-fetch('http://127.0.0.1:3000/data')
-    .then(res => res.json())
-    .then(data => console.log(data))
+fetch('/data')
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+fetch('/data/kismacska')
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+fetch('/data/201')
+  .then(res => res.json())
+  .then(data => console.log(data))
